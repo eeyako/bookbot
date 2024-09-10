5ed4576f44d89d8dd41e91a77f82596c662206d2
eeyako <eeyako42@gmail.com>
2024-09-09 22:07:42 -0700

    formatting

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import string
 import sys
+
 
 def main(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         words = count_words(file_contents)
         chars = [{k: v} for k, v in count_characters(file_contents).items()]
-        print(f'--- Begin report of {f.name} ---')
+        print(f"--- Begin report of {f.name} ---")
         print_report(words, chars)
-        print('--- End report ---')
+        print("--- End report ---")
 
 
 def print_report(words, chars):
     chars.sort(reverse=True, key=lambda n: list(n.values())[0])
-    print(f'{words} words found in the document\n')
+    print(f"{words} words found in the document\n")
     for char in chars:
         letter = list(char.keys())[0]
         count = list(char.values())[0]
@@ -35,9 +35,9 @@ def count_characters(text: str):
     return char_count
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 2:
-        raise Exception('No files were provided. Usage: python main.py {file_path}')
+        raise Exception("No files were provided. Usage: python main.py {file_path}")
 
     filepath = sys.argv[1]
     main(filepath)
